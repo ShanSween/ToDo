7c7a5e49597731f81eb892ce9ad3361dab8d9c67
ShanSween <shancsween@gmail.com>
2024-11-10 22:05:39 -0700

    add/remove todos

--- index.js
+++ index.js
@@ -13,11 +13,33 @@ function App(){
       isCompleted: false,
     }        
   ])
+const [value, setValue] = React.useState('');
+const handleSubmit = e =>{
+  e.preventDefault();
+  if (!value) return;
+  const newTodos = [...todos, {text:value, isCompleted: false}];
+  setTodos(newTodos);
+  setValue('');
+}
 
-  return(
+const removeTodo = e => {
+  const index = Number(e.target.id);
+  let temp = [...todos];
+  temp.splice(index,1);
+  setTodos(temp);
+}
+
+return(
     <>
-      {todos.map((todo, i) => <div className = "todo" key={i}>{todo.text}</div>)}
-
+      {todos.map((todo, i) => <div className = "todo" id={i}  onClick={removeTodo} key={i}>{todo.text}</div>)}
+      <form onSubmit={handleSubmit}>
+        <input 
+        type='text'
+        className="input"
+        value={value}
+        placeholder="Add todo..."
+        onChange={e => setValue(e.target.value)}/>
+      </form>
     </>
   );
 }
